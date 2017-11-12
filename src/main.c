3f1af0d0cc2f3e649b0523ba206a18c9f23ae472
JooJaeLin <c2522507@naver.com>
2017-11-13 02:52:22 +0900

    add signal handler

--- src/main.c
+++ src/main.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <unistd.h>
 
 #include "commands.h"
 #include "built_in.h"
 #include "utils.h"
+#include "signal_handlers.h"
+
+
 
 int main()
 {
+
   char buf[8096];
+
+  catch_sigint(SIGINT);
+  catch_sigtstp(SIGTSTP);
 
   while (1) {
     fgets(buf, 8096, stdin);
 
+   
     struct single_command commands[512];
     int n_commands = 0;
     mysh_parse_command(buf, &n_commands, &commands);
