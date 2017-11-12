831e5e1f7a76e9ad49dc9d48c348c05eb23d9fcf
JooJaeLin <c2522507@naver.com>
2017-11-13 02:51:04 +0900

    add process creation

--- src/commands.c
+++ src/commands.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "commands.h"
 #include "built_in.h"
 
@@ -37,11 +39,11 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
 
     int built_in_pos = is_built_in_command(com->argv[0]);
     if (built_in_pos != -1) {
-      if (built_in_commands[built_in_pos].command_validate(com->argc, com->argv)) {
-        if (built_in_commands[built_in_pos].command_do(com->argc, com->argv) != 0) {
-          fprintf(stderr, "%s: Error occurs\n", com->argv[0]);
+      if (built_in_commands[built_in_pos].command_validate(com->argc, com->argv)) {               if (built_in_commands[built_in_pos].command_do(com->argc, com->argv) != 0) {
+             fprintf(stderr, "%s: Error occurs\n", com->argv[0]);
         }
-      } else {
+      }
+      else {
         fprintf(stderr, "%s: Invalid arguments\n", com->argv[0]);
         return -1;
       }
@@ -49,13 +51,37 @@ int evaluate_command(int n_commands, struct single_command (*commands)[512])
       return 0;
     } else if (strcmp(com->argv[0], "exit") == 0) {
       return 1;
-    } else {
+    } 
+    else{
+	//process creation
+	pid_t  pid_r;
+	int pid = fork();
+	int status;
+	struct single_command* com = (*commands);
+
+	if(pid==-1){
+	    fprintf(stderr,"%s: Fork Fail\n",com->argv[0]);
+	    return -1;
+	}
+	else if(pid==0){
+	    pid_r=getpid();
+	    execv(com->argv[0],com->argv);
+	    fprintf(stderr,"%s: command not found\n",com->argv[0]);
+	    return-1;
+	}
+	else if(pid>0){
+	    pid_r=wait(&status);
+	}
+	
+    /////////////////////
+ /*   else {
       fprintf(stderr, "%s: command not found\n", com->argv[0]);
       return -1;
-    }
+    }*////////////////////
   }
 
   return 0;
+ }
 }
 
 void free_commands(int n_commands, struct single_command (*commands)[512])
